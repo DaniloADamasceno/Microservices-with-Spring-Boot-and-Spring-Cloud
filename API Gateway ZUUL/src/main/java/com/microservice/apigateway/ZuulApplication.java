1ae1465f00502d6cc13112cf705dedb8fd645d3b
DaniloADamasceno <danilo.engauto@gmail.com>
2023-04-26 12:05:57 -0300

    🛂 Criação do HR USER

--- API Gateway ZUUL/src/main/java/com/microservice/apigateway/ZuulApplication.java
+++ API Gateway ZUUL/src/main/java/com/microservice/apigateway/ZuulApplication.java
@@ -1,5 +1,6 @@
 package com.microservice.apigateway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -8,10 +9,14 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
-public class ApiGatewayZuulApplication {
+public class ZuulApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayZuulApplication.class, args);
+		SpringApplication.run(ZuulApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("✅ ✅API Gateway Zuul is running...✅ ✅");
+	}
 }
---
+++ Human Resources User/src/test/java/com/microservice/hruser/HumanResourcesUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
