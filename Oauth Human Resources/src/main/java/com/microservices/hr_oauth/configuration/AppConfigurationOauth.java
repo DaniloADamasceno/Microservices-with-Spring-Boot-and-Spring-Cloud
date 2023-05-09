13827dbfd4006dc02bdd6b38bba0ab6aa1ea96ad
DaniloADamasceno <danilo.engauto@gmail.com>
2023-05-09 12:19:23 -0300

    🐛 Configurações de segurança (Bug no Oauth🔖) resolvido -> Erro na Escrita do Termo no git

--- Oauth Human Resources/src/main/java/com/microservices/hr_oauth/configuration/AppConfigurationOauth.java
+++ Oauth Human Resources/src/main/java/com/microservices/hr_oauth/configuration/AppConfigurationOauth.java
@@ -1,19 +1,17 @@
 package com.microservices.hr_oauth.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-@RefreshScope      //-> Atualiza o contexto da aplicação com as novas propriedades
 @Configuration
 public class AppConfigurationOauth {
 
     @Value("${jwt.secret}")
-    private String jwtSecretKey;
+    private String jwtSecret;
     //?-----------------------------------------------  BEANS  ---------------------------------------------------------
     @Bean
     public BCryptPasswordEncoder passEncoder() {
@@ -23,7 +21,7 @@ public class AppConfigurationOauth {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey(jwtSecretKey);
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
 
